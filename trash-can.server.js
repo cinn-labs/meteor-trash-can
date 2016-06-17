9933665b7d984d590ee5f5c9c61d8be84fcb5f3f
Christian Storch <ccstorch@gmail.com>
2016-06-17 17:38:12 +0100

    Warn message

--- trash-can.server.js
+++ trash-can.server.js
@@ -2,6 +2,7 @@ import { Meteor } from 'meteor/meteor';
 import TrashCan from './trash-can.common';
 
 TrashCan.prepareCollection = function(collection, options) {
+  if(!collection.before) return console.warn(`[TRASH CAN] Collection ${collection._name} was not succesfully configured.`);
   collection.before.find(findHook);
   collection.before.findOne(findHook);
 }
