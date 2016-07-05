76316ae6d065093076b9549fc554b27434e132ae
Christian Storch <ccstorch@gmail.com>
2016-07-05 15:02:27 +0100

    0.0.5

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'cinn:trash-can',
-  version: '0.0.4',
+  version: '0.0.5',
   summary: 'Trash can for Meteor collection',
   git: 'https://github.com/cinn-labs/meteor-trash-can',
   documentation: 'README.md'
@@ -8,11 +8,11 @@ Package.describe({
 
 Package.onUse(function(api) {
   const both = ['client', 'server'];
-  api.versionsFrom('1.3.2.4');
+  api.versionsFrom('1.3.4');
   api.export('TrashCan');
 
   api.use('ecmascript');
-  api.use("matb33:collection-hooks@0.7.15");
+  api.use("matb33:collection-hooks@0.8.1");
 
   api.addFiles('trash-can.common.js', both);
   api.addFiles('trash-can.server.js', 'server');
